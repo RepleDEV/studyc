890f56541ef91546094af1e94e748bee19659435
RepleDEV <ragazzochaesa@gmail.com>
2024-01-03 17:05:11 +0700

    An oopsie

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -4,7 +4,7 @@ interface Props {
     onInput?: (input: string) => void;
 }
 
-export function SB(props: Props): React.ReactNode {
+export default function SearchBar(props: Props): React.ReactNode {
     return (
         <div className="bg-gray-200 p-2 px-4 rounded-full flex">
             <input
@@ -20,34 +20,4 @@ export function SB(props: Props): React.ReactNode {
             />
         </div>
     )
-}
-
-export default class SearchBar extends React.Component<Props> {
-    onInput?: (input: string) => void
-
-    constructor(props: Props) {
-        super(props);
-
-        this.onInput = props.onInput
-    }
-    render(): React.ReactNode {
-        return (
-            <div className="bg-gray-200 p-2 px-4 rounded-full flex">
-                <input
-                    type="text"
-                    className="w-full bg-inherit focus:outline-none"
-                    placeholder="Search"
-                    onInput={(e) =>  {
-                            e.stopPropagation();
-                            if (this.onInput)
-                                this.onInput(e.currentTarget.value)
-                        }
-                    }
-                />
-            </div>
-            // <div className="bg-gray-300 p-2 px-4 rounded-full">
-            //     <input type="text" className="w-full bg-gray-300 focus:outline-none" placeholder="Search"/>
-            // </div>
-        )
-    }
 }
