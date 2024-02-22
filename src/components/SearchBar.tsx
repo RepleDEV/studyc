3689b8886a9c6de3e887c7a5dacf6079e7ba2ea2
RepleDEV <ragazzochaesa@gmail.com>
2024-02-22 10:48:16 +0700

    Fix search bar text colors

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -9,7 +9,7 @@ export default function SearchBar(props: Props): React.ReactNode {
         <div className="flex h-[58px]">
             <input
                 type="text"
-                className="w-full border-4 border-dark_blue rounded-full focus:outline-none px-4 text-2xl placeholder:italic placeholder:lowercase"
+                className="w-full border-4 border-dark_blue rounded-full focus:outline-none px-4 text-2xl text-dark_blue placeholder:text-blue_light_text placeholder:italic placeholder:lowercase"
                 placeholder="Search for File"
                 onInput={(e) =>  {
                         e.stopPropagation();
