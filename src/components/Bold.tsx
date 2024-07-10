997707d18bd90b96a7f34ebfb39b2adc5f80f85e
RepleDEV <ragazzochaesa@gmail.com>
2024-07-10 10:04:00 +0700

    Recolored Bold and Link

--- src/components/Bold.tsx
+++ src/components/Bold.tsx
@@ -2,7 +2,7 @@ import React, { PropsWithChildren } from "react";
 
 export default function Bold({ children, ...props }: PropsWithChildren) {
     return <strong
-        className="font-bold text-purple-600"
+        className="font-bold text-dark_blue"
         {...props}
     >{children}</strong>
 }
--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -3,7 +3,7 @@ import React, { DetailedHTMLProps, AnchorHTMLAttributes } from "react";
 export default function Link({ children, className, ...props }: DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>): React.ReactNode {
     return (
         <a 
-            className={`${className} text-purple-400 visited:text-blue-600 underline font-semibold`}
+            className={`${className} text-purple visited:text-blue-600 underline font-semibold`}
             {...props}>
         {children}</a>
     )
