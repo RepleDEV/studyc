ebdf44168ae77c4b25f9cd56e4e231c95476cdec
RepleDEV <ragazzochaesa@gmail.com>
2024-07-25 13:22:58 +0700

    Fix invalid prop

--- src/components/md_components/list.tsx
+++ src/components/md_components/list.tsx
@@ -2,8 +2,8 @@ import * as React from "react";
 import type { Components } from "react-markdown/lib/ast-to-react";
 
 const list: Components = {
-    ul({children, ...props}) {
-        return <ul className="list-disc my-4 pl-8" {...props}>{children}</ul>
+    ul({children}) {
+        return <ul className="list-disc my-4 pl-8">{children}</ul>
     }
 }
 
