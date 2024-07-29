00099161b152884187cd17598ac48443283554b5
RepleDEV <ragazzochaesa@gmail.com>
2024-07-29 16:33:14 +0700

    Fix invalid math enclosing

--- src/modules/md_parser.ts
+++ src/modules/md_parser.ts
@@ -9,11 +9,39 @@ export default class MDPageParser {
 
 	constructor(raw: string) {
 		this.raw = raw;
+		this.converted = this.raw;
 		this.getMeta();
+		this.fixMathEnclosing();
+	}
+
+	/**
+	 * Checks if math enclosings ($$) have \n or <space> on either side & adds it when necessary
+	 */
+	fixMathEnclosing() {
+		const regex = /(?<!\n| )\$\$|\$\$(?!\n| )/g;
+		let text = this.converted
+		let m = regex.exec(text);
+
+		while (m !== null) {
+			if (m.index == regex.lastIndex) {
+				regex.lastIndex++;
+			}
+
+			let index = m.index;
+
+			// Checks if front side has newline
+			if (this.converted[m.index] == "\n")
+				index += 2
+
+			this.converted = this.converted.slice(0, index) + "\n" + this.converted.slice(index);
+			
+			text = this.converted
+			m = regex.exec(text);
+		}
 	}
 
 	getMeta() {
-		const m = matter(this.raw);
+		const m = matter(this.converted);
 		this.converted = m.content;
 		this.meta = m.data;
 	}
