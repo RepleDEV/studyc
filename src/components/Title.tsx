3c3cfb9d25e1131754a0695fc1432b3c822b9e24
RepleDEV <ragazzochaesa@gmail.com>
2024-07-30 19:54:27 +0700

    Added link to title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,14 +1,15 @@
 import React, { PropsWithChildren } from "react";
+import { Link } from "gatsby";
 
 export default function Title({ children, ...props }: PropsWithChildren) {
 	return (
 		<div className="flex h-[58px] items-center">
-			<span
+			<Link
 				className="text-4xl h-fit font-semibold lowercase tracking-tight text-purple"
-				{...props}
+				{...props} to="/"
 			>
 				{children}
-			</span>
+			</Link>
 		</div>
 	);
 }
