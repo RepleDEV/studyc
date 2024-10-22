2f408ab1a619f9161eda04abe2185ed0720aaa14
RepleDEV <ragazzochaesa@gmail.com>
2024-10-22 18:57:08 +0700

    Update margin on separator

--- src/pages/notes/{File.relativeDirectory}/{File.name}.tsx
+++ src/pages/notes/{File.relativeDirectory}/{File.name}.tsx
@@ -31,7 +31,7 @@ export default function Page(props: PageProps) {
             <div className="hidden md:flex flex-[0_0_200px]">
                 <Title>StudyC</Title>
             </div>
-            <div className="hidden md:flex bg-gray-400 h-[88%] mx-2 flex-[0_0_2px] self-center" />
+            <div className="hidden md:flex bg-gray-400 h-[88%] mr-4 flex-[0_0_2px] self-center" />
             <div className="flex flex-1 flex-col overflow-y-auto overflow-x-clip">
                 <div className="pt-5 pb-14 flex flex-1 flex-col overflow-y-auto overflow-x-clip">
                     <FileTitle>
