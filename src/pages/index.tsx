d382fdc6048c180f422d76662b6fb92acd56fd65
RepleDEV <ragazzochaesa@gmail.com>
2025-02-16 21:40:20 +0700

    adjust page divider margin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ function App(): React.ReactNode {
 					please suggest a better name for this project.
 				</div>
 			</div>
-            <div className="hidden md:flex bg-gray-400 h-[65%] mr-4 flex-[0_0_2px] self-center" />
+            <div className="hidden md:flex bg-gray-400 h-[65%] mx-2 flex-[0_0_2px] self-center"/>
 			<div className="flex flex-1 flex-col">
 				<Title className="md:hidden mb-5">StudyC</Title>
 				<SearchBar onInput={set_searchInput} />
