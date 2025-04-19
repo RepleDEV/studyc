0e65ad53fc46c0c61fb80534c7ff5059acd20ac8
RepleDEV <ragazzochaesa@gmail.com>
2025-04-19 17:44:06 +0700

    remove unnecessary message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,10 +11,7 @@ function App(): React.ReactNode {
 		<Layout>
 			<div className="hidden md:flex flex-col flex-[0_0_200px]">
 				<Title>StudyC</Title>
-				<div className="mt-auto mb-3 pr-3 leading-4">
-					studyc made by sesa.<br/><br/>
-					please suggest a better name for this project.
-				</div>
+				<div className="mt-auto mb-3 pr-3 leading-4"></div>
 			</div>
             <div className="hidden md:flex bg-gray-400 h-[65%] mx-2 flex-[0_0_2px] self-center"/>
 			<div className="flex flex-1 flex-col">
