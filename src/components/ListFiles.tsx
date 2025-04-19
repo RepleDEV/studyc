9bc9613408ba44643674148e6a9e4dcedd4ed94f
RepleDEV <ragazzochaesa@gmail.com>
2025-04-19 16:33:29 +0700

    implement sort icon indicator

--- src/components/ListFiles.tsx
+++ src/components/ListFiles.tsx
@@ -4,6 +4,9 @@ import { QueryResults, FileList, processFiles, File } from "../modules/listFiles
 import FileListing from "./FileListing";
 import Fuse from "fuse.js";
 
+import ArrowDown from "../../assets/svg/arrow_down.svg";
+import ArrowUp from "../../assets/svg/arrow_up.svg";
+
 function getListOfFiles() {
 	const query = graphql`
 		{
@@ -53,20 +56,26 @@ export default function ListFiles({ searchInput }: { searchInput?: string }) {
 	return (
 		<>
 			<div className="flex py-3 flex-row border-b-2 font-semibold select-none">
-				<span className="basis-2/5 cursor-pointer"
+				<span className="basis-2/5 cursor-pointer flex items-center"
 					onClick={(e) => {
 						e.stopPropagation();
 						setLastUpdateSort(0);
 						setFileNameSort(!fileNameSort ? 1 : fileNameSort * -1);
 					}}
-				>File name</span>
+				>File name
+				{fileNameSort < 0 && <ArrowDown className="w-5 inline-block" />}
+				{fileNameSort > 0 && <ArrowUp className="w-5 inline-block" />}
+				</span>
 				<span className="hidden md:block basis-32 cursor-pointer"
 					onClick={((e) => {
 						e.stopPropagation();
 						setFileNameSort(0);
 						setLastUpdateSort(!lastUpdateSort ? 1 : lastUpdateSort * -1);
 					})}
-				>Last Update</span>
+				>Last Update
+				{lastUpdateSort < 0 && <ArrowDown className="w-5 inline-block" />}
+				{lastUpdateSort > 0 && <ArrowUp className="w-5 inline-block" />}
+				</span>
 				<span className="ml-auto">Subject</span>
 			</div>
 			<div className="flex flex-1 flex-col pb-3 overflow-y-scroll">
