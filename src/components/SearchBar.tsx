ba4ebc4bf12558ef2c3ae198eb4f335120291ec4
RepleDEV <ragazzochaesa@gmail.com>
2025-04-21 12:22:02 +0700

    restyle search bar, add search icon

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Search from "../../assets/svg/search.svg";
 
 interface Props {
 	onInput?: (input: string) => void;
@@ -7,10 +8,11 @@ interface Props {
 
 export default function SearchBar(props: Props): React.ReactNode {
 	return (
-		<div className="flex h-[58px]">
+		<div className="flex h-[58px] border-b-2 border-gray-500">
+			<Search className="w-10 mx-2" />
 			<input
 				type="text"
-				className="w-full border-2 rounded-md border-orange-500 focus:outline-none px-4 text-2xl text-purple placeholder:purple placeholder:italic placeholder:lowercase"
+				className="w-full focus:outline-none text-2xl text-purple placeholder:purple placeholder:italic placeholder:lowercase"
 				placeholder={props.placeholder || "Search for File"}
 				onInput={(e) => {
 					e.stopPropagation();
